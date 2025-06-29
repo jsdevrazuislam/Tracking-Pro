db63d2535b6e4fb12b168187ac3c893ead5ed03e
jsdevrazuislam <razuislam.upwork@gmail.com>
2025-06-29 09:46:43 +0600

    fix/cors issue

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     unoptimized: true,
   },
   experimental: {
-    allowedDevOrigins: ['http://192.168.1.101:3000', 'http://192.168.8.76:3000', 'https://87f5-103-153-155-209.ngrok-free.app'],
+    allowedDevOrigins: ['http://192.168.1.101:3000', 'http://192.168.8.76:3000'],
   },
 }
 
